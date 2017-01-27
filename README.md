6beb77d0b1b26780b4fbae21caf7638adeac7fd3
Lifan Yang <yanglifan@gmail.com>
2017-01-27 22:05:15 +0800

    A simple go application

---
+++ README.md
@@ -0,0 +1,2 @@
+# How to run
+Add root dir into GOPATH.
---
+++ src/basic/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "mymath"
+
+func main() {
+	fmt.Printf("Hello, world. Sqrt(2) = %v\n\n", mymath.Sqrt(2))
+}
