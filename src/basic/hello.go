206744ca10cac009bd7f21a450980eb6a29e83a9
Lifan Yang <yanglifan@gmail.com>
2017-01-28 15:31:10 +0800

    Demo type convert

--- src/basic/hello.go
+++ src/basic/hello.go
@@ -5,10 +5,16 @@ import "mymath"
 
 const (
 	a = iota
-	b = iota
+	b
 )
 
 func main() {
 	fmt.Printf("Hello, world. Sqrt(2) = %v\n\n", mymath.Sqrt(2))
-	fmt.Printf("a=%d, b=%d", a, b)
+	fmt.Printf("a=%d, b=%d\n", a, b)
+
+	s := "hello"
+	c := []rune(s) // rune is int32 alias
+	c[0] = 'c'
+	s2 := string(c)
+	fmt.Printf("%s\n", s2)
 }
