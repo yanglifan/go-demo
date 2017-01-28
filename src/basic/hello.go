29a1a18cda282faf145a8c9d5326c8e4ea7943be
Lifan Yang <yanglifan@gmail.com>
2017-01-28 15:09:52 +0800

    Demo iota

--- src/basic/hello.go
+++ src/basic/hello.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 import "mymath"
 
+const (
+	a = iota
+	b = iota
+)
+
 func main() {
 	fmt.Printf("Hello, world. Sqrt(2) = %v\n\n", mymath.Sqrt(2))
+	fmt.Printf("a=%d, b=%d", a, b)
 }
