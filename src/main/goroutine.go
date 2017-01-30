ea7e9cd86ecf5e169bc3f9492502a5fdb5b373eb
Lifan Yang <yanglifan@gmail.com>
2017-01-30 14:02:08 +0800

    Demo channel

--- src/main/goroutine.go
+++ src/main/goroutine.go
@@ -14,5 +14,19 @@ func main() {
 	go ready("Tea", 2)
 	go ready("Coffee", 1)
 	fmt.Println("I'm waiting")
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
+
+	ci := make(chan int)
+	go readFromChannel(ci)
+	go writeIntoChannel(ci)
+	time.Sleep(2 * time.Second)
+}
+
+func readFromChannel(ci chan int) {
+	i := <-ci
+	fmt.Println("Read from channel ->", i)
+}
+
+func writeIntoChannel(ci chan int) {
+	ci <- 1
 }
